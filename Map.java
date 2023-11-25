5ba39b251775ce3e67a44bc409009c8f62796dd0
marmaxx <mariusfil0d0@gmail.com>
2023-11-25 18:54:21 +0800

    chgt map

--- Map.java
+++ Map.java
@@ -1,5 +1,5 @@
 public class Map {
-    public void contourMap(Cellule[][] cell){
+    /*public void contourMap(Cellule[][] cell){
         for (int i = 0; i < cell.length; i++){
             for (int j = 0; j < cell[0].length; j++){
                 if (i==0){ cell[i][j] = new Cellule("ARBRE");}
@@ -44,13 +44,61 @@ public class Map {
         }
         cell[6][4] = new Cellule("SABLE");
         afficher(cell);
+    }*/
+
+    public void contourMap(Cellule[][] cell){
+        for (int i = 0; i < cell.length; i++){
+            for (int j = 0; j < cell[0].length; j++){
+                if (i==0){ cell[i][j] = Cellule.sol(Cellule.Contenu.ARBRE);}
+                if (i < cell.length-1 && j == 0 || i < cell.length-1 && j == cell[0].length-1){ cell[i][j] =  Cellule.sol(Cellule.Contenu.ARBRE);}
+                if (i == cell.length-1 && j < 3){ cell[i][j] =  Cellule.sol(Cellule.Contenu.BASE_IA);}
+                if (i == cell.length-1 && j > cell[0].length-4){ cell[i][j] =  Cellule.sol(Cellule.Contenu.BASE_HUMAIN);}
+                if (i == cell.length-1 && j >= 3 && j<= cell[0].length-4){ cell[i][j] =  Cellule.sol(Cellule.Contenu.ARBRE);}
+            }
+        }
     }
+
+    public void map1 (){
+        Cellule [][] cell = new Cellule[8][9];
+        contourMap(cell);
+        for (int i = 1; i < cell[1].length-1; i++){
+            cell[1][i] =  Cellule.sol(Cellule.Contenu.SABLE);
+        }
+        cell[1][4] =  Cellule.sol(Cellule.Contenu.TERRE);
+        for (int i = 1; i < cell.length-1; i++){
+            cell[i][1] =  Cellule.sol(Cellule.Contenu.SABLE);
+        }
+        for (int i = 1; i < cell.length-1; i++){
+            cell[i][3] =  Cellule.sol(Cellule.Contenu.SABLE);
+        }
+        for (int i = 1; i < cell.length-1; i++){
+            cell[i][7] = Cellule.sol(Cellule.Contenu.SABLE);
+        }
+        for (int i = 1; i < cell.length-1; i++){
+            cell[i][5] =  Cellule.sol(Cellule.Contenu.SABLE);
+        }
+        for (int i = 2; i < cell.length-1; i++){
+            if (i%2==0){ cell[i][2] =  Cellule.sol(Cellule.Contenu.TERRE);}
+            else{ cell[i][2] =  Cellule.sol(Cellule.Contenu.EAU);}
+        }
+        for (int i = 2; i < cell.length-1; i++){
+            if (i%2==0){ cell[i][6] =  Cellule.sol(Cellule.Contenu.TERRE);}
+            else{ cell[i][6] =  Cellule.sol(Cellule.Contenu.EAU);}
+        }
+        for (int i = 1; i < cell.length-2; i++){
+            if (i%2!=0){ cell[i][4] =  Cellule.sol(Cellule.Contenu.TERRE);}
+            else{ cell[i][4] =  Cellule.sol(Cellule.Contenu.EAU);}
+        }
+        cell[6][4] = Cellule.sol(Cellule.Contenu.SABLE);
+        afficher(cell);
+    }
+
 
     public void map2(){
         Cellule [][] cell = new Cellule[8][9];
     }
 
-    public static void printColoredUnderscore(String s) {
+    public static void printColoredSymbole(char symbole, String s) {
         String ANSI_RESET = "\u001B[0m";
         String ANSI_RED = "\u001B[31m";
         String ANSI_GREEN = "\u001B[32m";
@@ -69,7 +117,7 @@ public class Map {
             case "blanc": couleur = ANSI_WHITE; break;
             default: couleur = ANSI_RESET;
         }
-        System.out.print(couleur + "_ " + ANSI_RESET);
+        System.out.print(couleur + symbole+" " + ANSI_RESET);
     }
     
 
@@ -77,14 +125,15 @@ public class Map {
         for (int i = 0; i < c.length; i++){
             for (int j = 0; j < c[0].length; j++){
                 if (c[i][j] != null){
-                    switch (c[i][j].getContenu()) {
-                        case "ARBRE": printColoredUnderscore("vert"); break;
-                        case "SABLE": printColoredUnderscore("jaune"); break;
-                        case "TERRE": printColoredUnderscore("blanc");; break;
-                        case "EAU": printColoredUnderscore("bleu");; break;
-                        case "BASE_IA": printColoredUnderscore("rouge");; break;
-                        case "BASE_HUMAIN": printColoredUnderscore("gris");; break;
-                        default: printColoredUnderscore("null");; break;
+                    Cellule.Contenu contenu =  c[i][j].getContenu();
+                    switch (contenu) {
+                        case ARBRE: printColoredSymbole('x',"vert"); break;
+                        case SABLE: printColoredSymbole(' ', "jaune"); break;
+                        case TERRE: printColoredSymbole('_',"blanc");; break;
+                        case EAU: printColoredSymbole('~',"bleu");; break;
+                        case BASE_IA: printColoredSymbole('|', "rouge"); break;
+                        case BASE_HUMAIN: printColoredSymbole('|', "gris");; break;
+                        default: break;
                     }
                 }
                 else { System.out.print("N ");}
