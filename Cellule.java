404012c884c05b54ecea512c4c31ea6a132a4275
marmaxx <mariusfil0d0@gmail.com>
2023-12-09 21:26:29 +0800

    ptite rectif

--- Cellule.java
+++ Cellule.java
@@ -8,9 +8,6 @@ public record Cellule (Contenu c , boolean dispo){
     public Contenu getContenu(){ return this.c;}
     public void setDisponible(boolean dispo){ disponible = dispo;}
     public void setMobs(boolean mob){ mobs = mob;}
-
-
-    
     
     public static Cellule sol (Contenu c){ return new Cellule(c,false);}
 
