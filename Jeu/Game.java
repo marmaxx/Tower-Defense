77c47d7a16328fa77b99f54dfded3db802b51130
marmaxx <mariusfil0d0@gmail.com>
2023-12-21 16:00:07 +0800

    maj

--- Jeu/Game.java
+++ Jeu/Game.java
@@ -3,16 +3,42 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Game implements KeyListener{
+    private int score, argent, vieBase;
     public static void main (String [] args){
-        Game game = new Game();
+        new Game();
     }
 
     private GameVue gameVue;
+
+    public GameVue getGameVue(){ return this.gameVue;}
     
     public Game(){
         this.gameVue = new GameVue(this);
         //gameVue.startGame();
     }
+
+    public void start(){
+        score = 0;
+        argent = 150;
+        vieBase = 3;
+        setScore(score);
+        setArgent(argent);
+        setVieBase(vieBase);
+        MobsSurLaMap.getInstance().reset();
+    }
+
+    public void setScore(int score){
+       this.score = score; 
+    } 
+
+    public void setArgent(int argent){
+        this.argent = argent;
+    }
+
+    public void setVieBase(int vieBase){
+        this.vieBase = vieBase;
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
         throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
